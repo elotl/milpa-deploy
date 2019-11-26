d54498b66a301e6630895e1fdeae6f7411455faf
Vilmos Nebehaj <v.nebehaj@gmail.com>
2019-11-26 15:52:26 -0800

    Create a CA for signing webhook certificate

--- deploy/create-webhook.sh
+++ deploy/create-webhook.sh
@@ -6,14 +6,6 @@ usage() {
     cat <<EOF
 Generate certificate suitable for use with a webhook service.
 
-This script uses k8s' CertificateSigningRequest API to a generate a certificate
-signed by k8s CA suitable for use with webhook services. This requires
-permissions to create and approve CSRs. See
-https://kubernetes.io/docs/tasks/tls/managing-tls-in-a-cluster for detailed
-explantion and additional instructions.
-
-The server key/cert k8s CA cert are stored in a k8s secret.
-
 usage: ${0} [OPTIONS]
 
 The following flags are required.
@@ -61,7 +53,11 @@ fi
 
 csrName=${service}.${namespace}
 tmpdir=$(mktemp -d)
-echo "creating certs in tmpdir ${tmpdir} "
+function cleanup {
+    rm -rf ${tmpdir}
+}
+trap cleanup EXIT
+echo "creating certs in tmpdir ${tmpdir}"
 
 cat <<EOF >> ${tmpdir}/csr.conf
 [req]
@@ -79,51 +75,12 @@ DNS.2 = ${service}.${namespace}
 DNS.3 = ${service}.${namespace}.svc
 EOF
 
+openssl req -nodes -x509 -newkey rsa:2048 -keyout ${tmpdir}/ca-key.pem -out ${tmpdir}/ca-cert.pem -subj "/C=US/ST=CA/L=San Francisco/O=Kubernetes/CN=Kiyot Webhook CA/emailAddress=info@elotl.co"
+
 openssl genrsa -out ${tmpdir}/server-key.pem 2048
 openssl req -new -key ${tmpdir}/server-key.pem -subj "/CN=${service}.${namespace}.svc" -out ${tmpdir}/server.csr -config ${tmpdir}/csr.conf
 
-# Clean up any previously created CSR for our service. Ignore errors if not present.
-kubectl delete csr ${csrName} 2>/dev/null || true
-
-# create server cert/key CSR and send to k8s API
-cat <<EOF | kubectl create -f -
-apiVersion: certificates.k8s.io/v1beta1
-kind: CertificateSigningRequest
-metadata:
-  name: ${csrName}
-spec:
-  groups:
-  - system:authenticated
-  request: $(cat ${tmpdir}/server.csr | base64 | tr -d '\n')
-  usages:
-  - digital signature
-  - key encipherment
-  - server auth
-EOF
-
-# verify CSR has been created
-while true; do
-    kubectl get csr ${csrName}
-    if [ "$?" -eq 0 ]; then
-        break
-    fi
-done
-
-# approve and fetch the signed certificate
-kubectl certificate approve ${csrName}
-# verify certificate has been signed
-for x in $(seq 300); do
-    serverCert=$(kubectl get csr ${csrName} -o jsonpath='{.status.certificate}')
-    if [[ ${serverCert} != '' ]]; then
-        break
-    fi
-    sleep 1
-done
-if [[ ${serverCert} == '' ]]; then
-    echo "ERROR: After approving csr ${csrName}, the signed certificate did not appear on the resource." >&2
-    exit 1
-fi
-echo ${serverCert} | openssl base64 -d -A -out ${tmpdir}/server-cert.pem
+openssl x509 -req -in ${tmpdir}/server.csr -CA ${tmpdir}/ca-cert.pem -CAkey ${tmpdir}/ca-key.pem -CAcreateserial -out ${tmpdir}/server-cert.pem
 
 # create the secret with CA cert and server cert/key
 kubectl create secret generic ${secret} \
@@ -132,7 +89,7 @@ kubectl create secret generic ${secret} \
         --dry-run -o yaml |
     kubectl -n ${namespace} apply -f -
 
-export CA_BUNDLE=$(kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[].cluster.certificate-authority-data}')
+export CA_BUNDLE="$(openssl base64 -in ${tmpdir}/ca-cert.pem | tr -d '\n')"
 
 manifest=$(cat <<'EOF'
 ---
@@ -185,7 +142,7 @@ webhooks:
         name: kiyot-webhook-svc
         namespace: kube-system
         path: "/mutate"
-      caBundle: ${CA_BUNDLE}
+      caBundle: "${CA_BUNDLE}"
     rules:
       - operations: [ "CREATE" ]
         apiGroups: [""]
