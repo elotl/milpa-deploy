aace4a3ffc4a70e274b045b1b2a6c1dcf8024381
Vilmos Nebehaj <v.nebehaj@gmail.com>
2020-01-22 13:55:35 -0800

    Add parameter for specifying certificate validity

--- deploy/create-webhook.sh
+++ deploy/create-webhook.sh
@@ -15,6 +15,7 @@ The following flags are required.
        --secret           Secret name for CA certificate and server certificate/key pair.
        --no-wait-for-control-plane
                           Don't wait for control plane services.
+       --valid-for-days   Specify number of days certificates will be valid for.
 EOF
     exit 1
 }
@@ -38,6 +39,10 @@ while [[ $# -gt 0 ]]; do
         --no-wait-for-control-plane)
             no_wait=true
             ;;
+        --valid-for-days)
+            valid_for_days="$2"
+            shift
+            ;;
         *)
             usage
             ;;
@@ -48,6 +53,7 @@ done
 [ -z ${service} ] && service=kiyot-webhook-svc
 [ -z ${secret} ] && secret=kiyot-webhook-certs
 [ -z ${namespace} ] && namespace=kube-system
+[ -z ${valid_for_days} ] && valid_for_days=1825
 
 if [ ! -x "$(command -v openssl)" ]; then
     echo "openssl not found"
@@ -68,6 +74,7 @@ echo "creating certs in tmpdir ${tmpdir}"
 
 cat <<EOF >> ${tmpdir}/csr.conf
 [req]
+days = $valid_for_days
 req_extensions = v3_req
 distinguished_name = req_distinguished_name
 [req_distinguished_name]
@@ -82,12 +89,12 @@ DNS.2 = ${service}.${namespace}
 DNS.3 = ${service}.${namespace}.svc
 EOF
 
-openssl req -nodes -x509 -newkey rsa:2048 -keyout ${tmpdir}/ca-key.pem -out ${tmpdir}/ca-cert.pem -subj "/C=US/ST=CA/L=San Francisco/O=Kubernetes/CN=Kiyot Webhook CA/emailAddress=info@elotl.co"
+openssl req -days $valid_for_days -nodes -x509 -newkey rsa:2048 -keyout ${tmpdir}/ca-key.pem -out ${tmpdir}/ca-cert.pem -subj "/C=US/ST=CA/L=San Francisco/O=Kubernetes/CN=Kiyot Webhook CA/emailAddress=info@elotl.co"
 
 openssl genrsa -out ${tmpdir}/server-key.pem 2048
-openssl req -new -key ${tmpdir}/server-key.pem -subj "/CN=${service}.${namespace}.svc" -out ${tmpdir}/server.csr -config ${tmpdir}/csr.conf
+openssl req -days $valid_for_days -new -key ${tmpdir}/server-key.pem -subj "/CN=${service}.${namespace}.svc" -out ${tmpdir}/server.csr -config ${tmpdir}/csr.conf
 
-openssl x509 -req -in ${tmpdir}/server.csr -CA ${tmpdir}/ca-cert.pem -CAkey ${tmpdir}/ca-key.pem -CAcreateserial -out ${tmpdir}/server-cert.pem
+openssl x509 -req -in ${tmpdir}/server.csr -CA ${tmpdir}/ca-cert.pem -CAkey ${tmpdir}/ca-key.pem -CAcreateserial -days $valid_for_days -out ${tmpdir}/server-cert.pem
 
 # create the secret with CA cert and server cert/key
 kubectl create secret generic ${secret} \
